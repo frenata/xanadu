01e0b61f3045cce09eb659440afddc4512545710
Andrew Nichols <andrew@frenata.net>
2023-09-28 21:05:15 -0400

    Update go program to query database and use go.mod

--- src/main.go
+++ src/main.go
@@ -2,13 +2,35 @@ package main
 
 import "log"
 import "fmt"
+//import "strconv"
 import "net/http"
-//import "github.com/lib/pq"
+import "database/sql"
+import _ "github.com/lib/pq"
+
+var pool *sql.DB
 
 func main() {
+	var err error
+	pool, err = sql.Open("postgres", "postgres://postgres@10.0.10.3/postgres?sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pool.Close()
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello")
+		var hits string
+		err = pool.QueryRow("select value from counter where key = 'hits'").Scan(&hits)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, err = pool.Exec("update counter set value = value + 1 where key = 'hits'")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, "Hello, there have been %v requests made to this server!\n", hits)
 	})
+
 
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
